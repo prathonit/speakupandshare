3dcaa286236c076dbdf18714149c44f8054d2f2a
= <=>
2020-07-31 22:17:52 +0530

    FIXES

--- lib/config.php
+++ lib/config.php
@@ -2,14 +2,14 @@
     /* SETTING TIMEZONE */
     date_default_timezone_set('Asia/Kolkata');
     /* DEFINING ADMIN USER */
-    define('ADMIN', 'admin@gmail.com');
+    define('ADMIN', 'admin@sus');
     /* CLASSES */
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/classes/database.class.php';
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/classes/auth.class.php';
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/classes/post.class.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/classes/database.class.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/classes/auth.class.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/classes/post.class.php';
 
     /* METHODS */
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/methods/hash.method.php';
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/methods/sanitize.method.php';
-    include_once $_SERVER['DOCUMENT_ROOT'].'/karan/lib/scripts/methods/post.method.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/methods/hash.method.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/methods/sanitize.method.php';
+    include_once $_SERVER['DOCUMENT_ROOT'].'/lib/scripts/methods/post.method.php';
 ?>
--- lib/scripts/classes/database.class.php
+++ lib/scripts/classes/database.class.php
@@ -2,7 +2,7 @@
     class Db{
         public function __construct() {
             $this->hostname = 'localhost';
-            $this->username = 'pks';
+            $this->username = 'root';
             $this->password = 'pwdpwd';
             $this->database = 'sus';
             $this->handle = new mysqli($this->hostname, $this->username, $this->password, $this->database);
